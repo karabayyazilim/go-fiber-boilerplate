24ed43b7b62aaa8baafe941431babaa83fa2747a
karabayyazilim <alikarabay77@gmail.com>
2024-12-31 12:22:37 +0300

    concurrent error fixed.

--- src/pkg/paginate/paginate.go
+++ src/pkg/paginate/paginate.go
@@ -9,13 +9,15 @@ type Paginator struct {
 	DB       *gorm.DB
 	Page     int
 	PageSize int
-	mutex    sync.Mutex
+	mutex    sync.RWMutex
 }
 
 func (p *Paginator) Paginate() *gorm.DB {
 	p.mutex.Lock()
 	defer p.mutex.Unlock()
 
+	dbCopy := p.DB.Session(&gorm.Session{NewDB: true})
+
 	offset := (p.Page - 1) * p.PageSize
-	return p.DB.Offset(offset).Limit(p.PageSize)
+	return dbCopy.Offset(offset).Limit(p.PageSize)
 }
